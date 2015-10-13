012435bfb048225414e89df9bd8372773f8a639d
orelkor <orelcor@yandex.ru>
2015-10-13 20:17:30 +0300

    Intial commit

---
+++ CashDesk/ViewModel/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashDesk.ViewModel
+{
+    public class Tile : Panel
+    {
+        public int TileNumber { get; private set; }
+        public string TileName { get; private set; }
+        public string TileCount { get; private set; }
+        public string TilePrice { get; private set; }
+        public string TileTotal { get; private set; }
+
+        //public Tile()
+        //{
+        //    Controls.Add(new Label());
+        //    Location = new Point();
+        //}
+    }
+}
---
+++ CashDesk/ViewModel/TileList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashDesk.ViewModel
+{
+    class TileList : Panel
+    {
+
+        private List<Tile> tileList;
+
+                    
+        public void AddTile(Tile tile)
+        {
+
+        }
+
+        public void DeleteTile(Tile tile)
+        {
+            
+        }
+                
+    }
+}
